cb1b2f3baeb550e9d0ffd81d51eed9d41bc3dedd
Love Khatri <lovekhatri9211@gmail.com>
2024-07-18 12:25:30 +0530

    Add Logout Button and User Button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,21 @@
-import { auth, signOut } from "@/auth"
+"use client";
 
-const SettingsPage = async () => {
-    const session = await auth();
-    
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import { useSession } from "next-auth/react";
+
+const SettingsPage = () => {
+    const user = useCurrentUser();
+
+    const onClick = () => {
+        logout();
+    }
+
     return (
-        <div className="w-full text-wrap">
-            {JSON.stringify(session)}
-            <form action={async () => {
-                "use server";
-
-                await signOut()
-            }}>
-                <button>
-                    Sign out
-                </button>
-            </form>
+        <div className="bg-white p-10 rounded-xl">
+            <button onClick={onClick}>
+                Sign out
+            </button>
         </div>
     )
 }
